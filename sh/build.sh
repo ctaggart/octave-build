1fc6b08e782b073362f9d7b1bdbe2896f71dfe92
Cameron Taggart <cameron.taggart@gmail.com>
2017-08-01 23:01:00 +0300

    try building with docker on travis ci

---
+++ sh/build.sh
@@ -0,0 +1,17 @@
+apt-get update
+apt-get install -y libtool
+apt-get install -y autoconf
+apt-get install -y make
+apt-get install -y g++
+apt-get install -y gfortran
+apt-get install -y libblas-dev liblapack-dev
+apt-get install -y libreadline-dev
+apt-get install -y gperf
+apt-get install -y bison
+apt-get install -y sed
+apt-get install -y mercurial
+
+cd /octave
+./configure
+make
+make clean
