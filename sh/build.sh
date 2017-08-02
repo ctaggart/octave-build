a60dcdb916a18becbbb82ddd1a4e7dc38fe2c538
Cameron Taggart <cameron.taggart@gmail.com>
2017-08-02 16:53:29 +0300

    build-dep

--- sh/build.sh
+++ sh/build.sh
@@ -3,17 +3,23 @@
 # http://wiki.octave.org/Talk:Build_from_source
 
 apt-get update
-apt-get install -y libtool
-apt-get install -y autoconf
-apt-get install -y make
-apt-get install -y g++
-apt-get install -y gfortran
-apt-get install -y libblas-dev liblapack-dev
-apt-get install -y libreadline-dev
-apt-get install -y gperf
+
+# apt-get install -y libtool
+# apt-get install -y autoconf
+# apt-get install -y make
+# apt-get install -y g++
+# apt-get install -y gfortran
+# apt-get install -y libblas-dev liblapack-dev
+# apt-get install -y libreadline-dev
+
+# apt-get install -y gperf
+# apt-get install -y bison
+# apt-get install -y sed
+# apt-get install -y mercurial
+
+apt-get build-dep -y octave
 apt-get install -y bison
 apt-get install -y sed
-apt-get install -y mercurial
 
 # cd /octave
 # ./bootstrap
